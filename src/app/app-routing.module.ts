99ebdea24f5ea128f2b10fe178cffaeb2cb90ed8
yyogesh <yogeshggn1@gmail.com>
2020-09-22 18:47:41 +0530

    angular lazy routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './modules/general/home/home.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'contact',
+    loadChildren: () => import('./modules/general/contact/contact.module').then(x => x.ContactModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./modules/general/about/about.module').then(x => x.AboutModule)
+  },
+  {
+    path: 'signin',
+    loadChildren: () => import('./modules/general/signin/signin.module').then(x => x.SigninModule)
+  },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/general/home/home.component';
+import { ContactComponent } from './modules/general/contact/contact.component';
+import { AboutComponent } from './modules/general/about/about.component';
+import { SigninComponent } from './modules/general/signin/signin.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+import { ContactModule } from './modules/general/contact/contact.module';
+import { SigninModule } from './modules/general/signin/signin.module';
+import { AboutModule } from './modules/general/about/about.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    SigninComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    ContactModule,
+    SigninModule,
+    AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
